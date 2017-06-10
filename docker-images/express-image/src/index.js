f4de602d33f8328399952b27c6b5ddfd784c283c
Guillaume Milani <milani.guillaume@gmail.com>
2017-06-10 13:48:21 +0200

    Return list of locations

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -5,37 +5,34 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send(generateStudents());
+    res.send(generateLocations());
 });
 
 app.listen(3000, function() {
     console.log("Accepting HTTP requests on port 3000");
 });
 
-function generateStudents() {
-    var numberOfStudents = chance.integer({
+function generateLocations() {
+    var numberOfLocations = chance.integer({
         min: 0,
         max: 10
     });
-    console.log(numberOfStudents);
-    var students = [];
-    for (var i = 0; i < numberOfStudents; i++) {
-        var gender = chance.gender();
-        var birthYear = chance.year({
-            min: 1986,
-            max: 1996
+    console.log(numberOfLocations);
+    var locations = [];
+    for (var i = 0; i < numberOfLocations; i++) {
+        var country = 'us';
+        var state = chance.state({
+            full: true,
+            country: country
         });
-        students.push({
-            firstName: chance.first({
-                gender: gender
+        locations.push({
+            city: chance.city({
+                state: state
             }),
-            lastName: chance.last(),
-            gender: gender,
-            birthday: chance.birthday({
-                year: birthYear
-            })
+            altitude: chance.altitude(),
+            state: state
         });
     };
-    console.log(students);
-    return students;
+    console.log(locations);
+    return locations;
 }
